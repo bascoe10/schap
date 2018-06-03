5dde2f0b3ee7d89ab128997b4a972bc27bb39fd8
Sesay, Abass <abass_sesay@comcast.com>
2018-06-03 12:10:01 -0400

    intial commit

---
+++ client.py
@@ -0,0 +1,228 @@
+import argparse
+import socket
+import random
+import rsa
+
+SERVER = []
+KEYPAIR = []
+MESSAGE_SESSIONS = {} #holds a list of users that are currently chatting with
+
+def gen_key():
+    sock = connect_to_server()
+    message = "KEY {0}\r\n".format(KEYPAIR[0].save_pkcs1())
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+    return
+
+def send_encrypted_message():
+    user = raw_input("Enter username: ")
+    #get public key
+    sock = connect_to_server()
+    message = "LKUP {0}\r\n".format(user)
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+    code, message =  response.split(" ", 1)
+    print code
+    if code != '000':
+        print "Cannot retrieve key"
+        return
+
+    key = rsa.PublicKey.load_pkcs1(message.split('#', 1)[1])
+
+    user_session = MESSAGE_SESSIONS.get(user) #check is user is in active list
+    if not user_session: # if they are not then add them
+        user_session = {
+            'message_id': random.randint(1,65536),
+            'message_2_ack': []
+        }
+        MESSAGE_SESSIONS[user] = user_session
+
+    message = "Hello" #raw_input("Enter message: ")
+    message = message.encode('utf8')
+    crypt = rsa.encrypt(message, key)
+    sock = connect_to_server()
+    message = "SENDMSR {0}#{1}#{2} {3}\r\n".format(user, user_session['message_id'], "", crypt)
+    print message
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+    user_session['message_id'] = user_session['message_id'] + 1
+    MESSAGE_SESSIONS[user] = user_session
+
+def send_message():
+    user = raw_input("Enter username: ")
+    user_session = MESSAGE_SESSIONS.get(user) #check is user is in active list
+    if not user_session: # if they are not then add them
+        user_session = {
+            'message_id': random.randint(1,65536),
+            'message_2_ack': []
+        }
+        MESSAGE_SESSIONS[user] = user_session
+
+    message = "Hello" #raw_input("Enter message: ")
+    sock = connect_to_server()
+    message = "SENDMSR {0}#{1}#{2} {3}\r\n".format(user, user_session['message_id'], "", message)
+    print message
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+    user_session['message_id'] = user_session['message_id'] + 1
+    MESSAGE_SESSIONS[user] = user_session
+
+def logout():
+    sock = connect_to_server()
+    message = "QUIT\r\n"
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+
+def check_server_version():
+    sock = connect_to_server()
+    message = "VERSION\r\n"
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+
+def ping_server():
+    sock = connect_to_server()
+    message = "PING\r\n"
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+
+def lookup_user():
+    username = raw_input("Enter username: ")
+    sock = connect_to_server()
+    message = "LKUP {0}\r\n".format(username)
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+
+def list_users():
+    sock = connect_to_server()
+    message = "LIST\r\n"
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+
+def connect_to_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER[0], SERVER[1]))
+    return sock
+
+def help_menu():
+    print("List of commands")
+    print("[1] List connect users")
+    print("[2] Lookup user")
+    print("[3] Ping server")
+    print("[4] Check server version")
+    print("[5] Send message")
+    print("[6] Send Encrypted Message")
+    print("[7] Help")
+    print("[8] Log Out")
+    print("[9] Quit")
+    print('[10] Generate encryption key')
+
+def welcome_sequence():
+    sock = connect_to_server()
+    # username = raw_input("Enter username: ")
+    # message = "USER {0}\r\n".format(username)
+    sock.send("VERSION\r\n")
+    response = sock.recv(1024)
+    print response
+    message = "USER abass\r\n"
+    sock.send(message)
+    response = sock.recv(1024)
+    print response
+    response_code, response_message = response.split(' ', 1)
+    if int(response_code) == 000:
+        # password = raw_input("Enter password: ")
+        # message = "PASS {0}\r\n".format(password)
+        message = "PASS abass\r\n"
+        sock.send(message)
+        response = sock.recv(1024)
+        print response
+        response_code, response_message = response.split(' ', 1)
+        if int(response_code) == 000:
+            # port = raw_input("Enter port: ")
+            # message = "PORT {0}\r\n".format(port)
+            message = "PORT 1045\r\n"
+            sock.send(message)
+            response = sock.recv(1024)
+            print response
+            gen_key()
+            return True
+    return False
+
+
+
+def client_repl():
+    #returns a boolean value to determine if a connection was established
+    connection_active = welcome_sequence()
+    help_menu()
+    while connection_active:
+        user_input = raw_input('> ')
+        user_input = int(user_input)
+        if user_input == 1:
+            list_users()
+        elif user_input == 2:
+            lookup_user()
+        elif user_input == 3:
+            ping_server()
+        elif user_input == 4:
+            check_server_version()
+        elif user_input == 5:
+            send_message()
+        elif user_input == 6:
+            send_encrypted_message()
+        elif user_input == 7:
+            help_menu()
+        elif user_input == 8:
+            logout()
+            print 'Session Closed'.center(40, '*')
+            client_repl()
+        elif user_input == 9:
+            print 'Good bye!'.center(40, '*')
+            if connection_active:
+                logout()
+            connection_active = False
+        elif user_input == 10:
+            gen_key()
+        else:
+            print 'Please select a valid option. Press [7] for help'
+
+    return
+
+# using argparse module process the command line arguments
+def get_cli_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('hostname', metavar='Hostname', type=str,
+                        help='this could be d hostname or ip address of the server')
+    parser.add_argument('port', metavar='port', type=int, default=21, nargs='?',
+                        help='an integer representing the port number')
+
+    args = parser.parse_args()
+    # check the validity of the hostname passed in the process convert the host name to ip addr
+    try:
+        host = socket.gethostbyname(args.hostname)
+    except:
+        return False
+
+    return {'hostname': host, 'port': args.port}
+
+
+def main():
+    args = get_cli_args()
+    SERVER.append(args['hostname'])
+    SERVER.append(args['port'])
+    print SERVER
+    (pubkey, privkey) = rsa.newkeys(512)
+    KEYPAIR.append(pubkey)
+    KEYPAIR.append(privkey)
+    print "Keys generated"
+    client_repl()
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,308 @@
+import socket
+import argparse
+import shelve
+import hashlib
+
+VERSION = '1.0'
+DB = shelve.open('users')
+CURRENT_USERS = {}
+HOSTNAME_TO_USER = {}
+ADMINS = {}
+# NEXT_TO_RECIEVE = []
+
+class ServerHandler(object):
+    def __init__(self, connection):
+        self.socket = connection[0]
+        self.address = connection[1]
+        self.next_to_recieve = []
+
+    '''
+    parse out the message the message to get command
+    and the argument. The method that handles the
+    command will parse out the argument list accordingly
+    '''
+    def __parse_message(self, message):
+        message = message.rstrip() #drops the delimeter \r\n
+        parsed_message = message.split(' ', 1)
+        if parsed_message[0].startswith('#'):
+            parsed_message = parsed_message[1].split(' ', 1)
+        if len(parsed_message) == 1:
+            parsed_message.append(None)
+        return(parsed_message)
+
+    '''
+    This a default method handles requests for command
+    that have not been Implemented.
+    '''
+    def __not_implemented(self, args):
+        self.__send_response("001 Command not Implemented")
+        return True
+
+    def __send_response(self, message):
+        try:
+            self.socket.send("{0}\r\n".format(message))
+        except:
+            return
+
+    '''
+    USER
+    message use to authenticate/registration a user
+    '''
+    def _user(self, args):
+        print 'user received'
+        if args == None: # if no argument is passed
+            self.__send_response("100 No username passed")
+            return
+        if args in CURRENT_USERS.keys(): # check if user has already been authenticated
+            self.__send_response("100 User already authenticated")
+            return
+        if args in DB.keys(): # if use exist in our database
+            self.__send_response("000 User found please send password")
+            self.user_registration = False
+        else: #user does not exist in our database
+            self.__send_response("001 User not found sending password would create user")
+            self.user_registration = True
+        self.username = args
+        self.next_to_recieve.remove('USER')
+        self.next_to_recieve.append('PASS')
+        return
+
+    # TODO: Fill in method
+    def _pass(self, args):
+        print 'pass received'
+        if args == None:
+            self.__send_response("100 No password passed")
+            return
+        sha_digest = hashlib.sha256(args).digest()
+        password_hash = sha_digest.encode('hex')
+
+        if self.user_registration:
+            DB[self.username] = password_hash
+            HOSTNAME_TO_USER[self.address[0]] = self.username
+            DB.sync()
+            self.__send_response("000 User authenticated")
+        else:
+            if DB[self.username] == password_hash:
+                HOSTNAME_TO_USER[self.address[0]] = self.username
+                self.__send_response("000 User authenticated")
+            else:
+                self.__send_response("001 Bad password")
+        self.next_to_recieve.remove('PASS')
+        return
+
+
+    # TODO: Fill in method
+    def _port(self, args):
+        if args == None: # if no argument is passed
+            self.__send_response("100 No port passed")
+            return
+        hostname = self.address[0]
+        username = HOSTNAME_TO_USER[hostname]
+        CURRENT_USERS[self.username] = {'address': (hostname, args)}
+        self.__send_response("000 User port added")
+        self.socket.close()
+        self.socket = None
+
+    # TODO: Fill in method
+    def _key(self, args):
+        if args == None: # if no argument is passed
+            self.__send_response("100 No key passed")
+            return
+        username = HOSTNAME_TO_USER[self.address[0]]
+        CURRENT_USERS[username]['key'] = args
+        self.__send_response("000 User encryption key created")
+        self.socket.close()
+        self.socket = None
+
+    # TODO: Fill in method
+    def _list(self, args):
+        current_users = CURRENT_USERS.keys()
+        self.__send_response("001 " + ','.join(current_users))
+        self.socket.close()
+        self.socket = None
+        return
+
+    # TODO: Fill in method
+    def _lkup(self, args):
+        current_users = CURRENT_USERS.keys()
+        if args in current_users:
+            self.__send_response("000 {0}#{1}".format(args, CURRENT_USERS[args]['key']))
+        else:
+            self.__send_response("001 User not found")
+        self.socket.close()
+        self.socket = None
+
+    # TODO: Fill in method
+    def _ping(self, args):
+        self.__send_response("000 Pong")
+        self.socket.close()
+        self.socket = None
+
+    # TODO: Fill in method
+    def _sendmsr(self, args):
+        print args
+        user = args.split('#', 1)[0]
+        current_users = CURRENT_USERS.keys()
+        if user in current_users:
+            self.__send_response("000 Message received")
+        else:
+            self.__send_response("001 User not found")
+        self.socket.close()
+        self.socket = None
+
+    # TODO: Fill in method
+    def _encryptmsg(self, args):
+        print args
+        user = args.split('#', 1)[0]
+        current_users = CURRENT_USERS.keys()
+        if user in current_users:
+            self.__send_response("000 Message received")
+        else:
+            self.__send_response("001 User not found")
+        self.socket.close()
+        self.socket = None
+
+    # TODO: Fill in method
+    def _confmsg(self, args):
+        return
+
+    # TODO: Fill in method
+    def _broadcast(self, args):
+        return
+
+    # TODO: Fill in method
+    def _admin(self, args):
+        username = HOSTNAME_TO_USER[self.address[0]]
+        if username in ADMINS:
+            self.__send_response("000 Admin mode activated")
+        else:
+            self.__send_response("001 You do not have admin access")
+        self.socket.close()
+        self.socket = None
+        return
+
+    # TODO: Fill in method
+    def _addadmin(self, args):
+        username = HOSTNAME_TO_USER[self.address[0]]
+        if username in ADMINS:
+            if args in DB.keys():
+                self.__send_response("000 User granted admin access")
+            else:
+                self.__send_response("001 User not found")
+        else:
+            self.__send_response("001 You do not have admin access")
+        self.socket.close()
+        self.socket = None
+        return
+
+    # TODO: Fill in method
+    def _kick(self, args):
+        user = CURRENT_USERS.pop(args)
+        HOSTNAME_TO_USER.pop(user['address'][0])
+        self.__send_response("000 User removed")
+        self.socket.close()
+        self.socket = None
+        return
+
+    # TODO: Fill in method
+    def _deleteuser(self, args):
+        user = CURRENT_USERS.pop(args)
+        del DB[args]
+        DB.sync()
+        HOSTNAME_TO_USER.pop(user['address'][1])
+        self.__send_response("000 User removed")
+        self.socket.close()
+        self.socket = None
+        return
+
+
+    '''
+    VERSION
+    this command is used to query the server of the version
+    it is running
+    '''
+    def _version(self, args):
+        print 'version command received'
+        self.__send_response("000 "+VERSION)
+        if self.address[0] in HOSTNAME_TO_USER.keys():
+            self.socket.close()
+            self.socket = None
+        else:
+            self.next_to_recieve.append('USER')
+        return
+
+    '''
+    QUIT command
+    '''
+    def _quit(self, args):
+        print 'quit command received'
+        # remove user from list of connected users
+        username = HOSTNAME_TO_USER.pop(self.address[0])
+        user = CURRENT_USERS.pop(username)
+        self.__send_response("000 Closing connection")
+        self.socket.close()
+        self.socket = None
+        return
+
+    '''
+    handles request after a connection is established
+    '''
+    def handle(self):
+        while self.socket:
+            message = ""
+            while not message.endswith('\r\n'):
+                message += self.socket.recv(1024)
+            command, arguments = self.__parse_message(message)
+            #check if command is valid to be recieved
+            if len(self.next_to_recieve) != 0 and not(command in self.next_to_recieve):
+                print "Next to recieve {0}".format(",".join(self.next_to_recieve))
+                print "Recieved {0}".format(command)
+                self.socket.send('001 In wrong state\r\n')
+                self.socket.close()
+                break
+            '''
+            user is not required to be logged in to send
+            VERSION, USER or PASS commands
+            '''
+            if command == 'VERSION' or command in self.next_to_recieve or self.address[0] in  HOSTNAME_TO_USER.keys():
+                command = command.lower()
+                func = getattr(self, '_'+command, self.__not_implemented)
+                func(arguments)
+            else:
+                self.socket.send('001 Please proper auth steps authenticate\r\n')
+                self.socket.close()
+                break
+
+
+
+
+def get_cli_args():
+    parser = argparse.ArgumentParser(description='Please pass the required arguments')
+    parser.add_argument('port', metavar='port', type=int,
+                        help='an integer representing the port number')
+
+    args = parser.parse_args()
+    host = ''
+    return {'hostname': host, 'port': args.port}
+
+def main():
+    p_args = get_cli_args()
+    if not p_args:
+        print "Cannot resolve hostname"
+        exit(0)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((p_args['hostname'], p_args['port']))
+
+    while True:
+        print 'Logged in users'
+        print CURRENT_USERS
+        print HOSTNAME_TO_USER
+        print 'end'
+        sock.listen(1)
+        client = sock.accept()
+        print client
+        handler = ServerHandler(client)
+        handler.handle()
+
+if __name__ == '__main__':
+    main()
